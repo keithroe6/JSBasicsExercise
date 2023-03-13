125ecb5420e536ed5a3cc9e92675e7b56b2714f3
Keith Roe <kdroe1@gmail.com>
2023-03-13 15:38:26 -0400

    js basics exercise

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// 1.
+console.log(13);
+// 2. five data types
+//  Numbers, Strings, Boolean, Null, and Undefined
+
+//3. six operators
+// + Addition, - Subtraction, * Multiplication, / Division, ** Exponentiation, % Modulus
+
+// 4. Two ways to use modulus to get a 1 out of a 5.
+console.log(5 % 2);
+console.log(5%4);
+
+// 5. Mary Poppins string
+console.log('I\'M MARY POPPINS Y\'ALL');
+
+// 6. concatenation of Hawk and eye
+console.log('Hawk' + 'eye');
+
+// 7. How many letters in my name
+console.log('Keith'.length + 'Roe'.length);
+
+//8. Using Index
+console.log('Avengers: Endgame'[16]);
+
+// Bonus 1.
+console.log('\\');
+
+// Bonus 2.
+console.log(5**4-9);
+
+// Bonus 3.
+console.log('I'.length - 'Am'.length + 'Iron'.length - 'Man'.length);
+
+// Bonus 4.
+console.log('CAP'[1]);
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('NICK FURY'[0]);
+console.log('GROOT'[0]);
+console.log('DOCTOR STRANGE'[13]);
+console.log('THOR'[3]);
+console.log('STAR-LORD'[0]);
+
+// Bonus 5.
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
